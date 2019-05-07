2effbdb2cc8ef1cf76b4d7c815ccc06c8e6b25ff
Arnaud GOULPEAU <arnaud.goulpeau@guestapp.me>
2019-05-07 13:44:58 +0200

    fix container init
    - use CDNs for external front libs
    - remove buggous dependency
    - rework way data is loaded at container start

--- docker/images/dev/init.sh
+++ docker/images/dev/init.sh
@@ -9,9 +9,13 @@ php bin/console assets:install web
 /srv/wait-for-mariadb.sh
 
 # set database and fixtures
-php bin/console doctrine:schema:update --force --no-interaction
+
+php bin/console doctrine:database:drop --no-interaction --force
+php bin/console doctrine:database:create --no-interaction
 
 mysql --host=mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" musicpractise < /srv/app/var/dumps/datadump.sql
+
+php bin/console doctrine:schema:update --force --no-interaction
 
 # start server
 php bin/console server:run 0.0.0.0:8000
